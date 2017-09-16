f8f7a268b814fd133d1148524c1cd959870cc57a
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2017-09-15 23:40:40 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "Stack $1 is being created"
 cat docker-compose.yml | sed -e "s/{{ .Stack.Name }}/$1/g" > docker-compose-built.yml
-../rancher --url "$2" --access-key "$3" --secret-key "$4" stack create -f ./docker-compose-built.yml -r ./rancher-compose.yml
+../rancher --url "$2" --access-key "$3" --secret-key "$4" stack create $1 -f ./docker-compose-built.yml -r ./rancher-compose.yml
