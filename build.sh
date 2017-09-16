3b0b6999b2d2ca8f77779170704a0deb264d2b0d
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2017-09-15 23:09:46 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "Stack $1 is being created"
 cat docker-compose.yml | sed -e "s/{{ .Stack.Name }}/$1/g" > docker-compose-built.yml
-./rancher up -s $1 -f ./docker-compose-built.yml --rancher-file ./rancher-compose.yml
+../rancher-compose -f ./docker-compose-built.yml -r ./rancher-compose.yml -p $1 create
